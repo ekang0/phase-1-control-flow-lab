65d57ae3f7ff9efe44e37d494f5732c02cf340cb
Esther Kang <eskang87@gmail.com>
2021-11-24 12:13:22 -0800

    commit tree d1adf4101725d9237de3dd17fb846e4e13cda10e

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
   // Write your code here!
-}
+  if (rideDistance <= 400) {
+    return 'This one is on me!';
+  } else if (rideDistance > 2000 && rideDistance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (rideDistance > 2500) {
+    return 'No can do.';
+  }
+};
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
-}
+  /* if (cityName === 'NYC') {
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  } */
+  return cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+};
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous' : 
+      return 'Thank you so much.';
+      break;
+    case 'not as generous' :
+      return 'Thank you.';
+      break;
+    default :
+      return 'Bye.';
+      break;
+}
 }
